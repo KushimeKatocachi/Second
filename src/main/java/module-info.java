2deed02e4c94d629dd69f71f8db8450422f2b23a
Erkanat <tashenov05@gmail.com>
2023-05-28 21:51:36 +0600

    BookStore

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.programmersclub.bookstore.bookstore {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.programmersclub.bookstore.bookstore to javafx.fxml;
+    exports com.programmersclub.bookstore.bookstore;
+}
